17327ed8e3cb22a29e46dc825bf0655715869cc5
Jenny Saunders <jennygloversaunders@gmail.com>
2024-12-09 06:33:53 -0500

    refactor(package json): add commands to deploy to gh pages

---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,12 @@
+import './ToggleSwitch.css';
+
+function ToggleSwitch(){
+
+    return (
+     <div>
+        <input type="checkbox" name="" id="" />
+     </div>
+    );
+}
+
+export default ToggleSwitch;
